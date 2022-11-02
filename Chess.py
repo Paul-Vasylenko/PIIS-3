6bdcf628359e4db644cc31c171435f9aec52e19e
Paul-Vasylenko <maincrafter1307@gmail.com>
2022-11-02 23:56:29 +0200

    rewrite 3 algos

--- Chess.py
+++ Chess.py
@@ -1,115 +1,8 @@
 import chess
-import random as rd
-from constants import pawntable, knightstable, bishopstable, bookstable, rookstable, queenstable, kingstable
+from Negamax import NegamaxAgent
+from Negascout import NegascoutAgent
+from PVS import PVSAgent
 
-class AIEngine:
-
-    def __init__(self, board: chess.Board, maxDepth, color):
-        self.board = board
-        self.maxDepth = maxDepth
-        self.color = color
-
-    def evaluate(self):
-        board = self.board
-        if board.is_checkmate():
-            if board.turn:
-                return -9999
-            else:
-                return 9999
-        if board.is_stalemate():
-            return 0
-        if board.is_insufficient_material():
-            return 0
-        
-        wp = len(board.pieces(chess.PAWN, chess.WHITE))
-        bp = len(board.pieces(chess.PAWN, chess.BLACK))
-        wn = len(board.pieces(chess.KNIGHT, chess.WHITE))
-        bn = len(board.pieces(chess.KNIGHT, chess.BLACK))
-        wb = len(board.pieces(chess.BISHOP, chess.WHITE))
-        bb = len(board.pieces(chess.BISHOP, chess.BLACK))
-        wr = len(board.pieces(chess.ROOK, chess.WHITE))
-        br = len(board.pieces(chess.ROOK, chess.BLACK))
-        wq = len(board.pieces(chess.QUEEN, chess.WHITE))
-        bq = len(board.pieces(chess.QUEEN, chess.BLACK))
-        
-        material = 100*(wp-bp)+320*(wn-bn)+330*(wb-bb)+500*(wr-br)+900*(wq-bq)
-        
-        pawnsq = sum([pawntable[i] for i in board.pieces(chess.PAWN, chess.WHITE)])
-        pawnsq= pawnsq + sum([-pawntable[chess.square_mirror(i)] 
-                                        for i in board.pieces(chess.PAWN, chess.BLACK)])
-        knightsq = sum([knightstable[i] for i in board.pieces(chess.KNIGHT, chess.WHITE)])
-        knightsq = knightsq + sum([-knightstable[chess.square_mirror(i)] 
-                                        for i in board.pieces(chess.KNIGHT, chess.BLACK)])
-        bishopsq= sum([bishopstable[i] for i in board.pieces(chess.BISHOP, chess.WHITE)])
-        bishopsq= bishopsq + sum([-bishopstable[chess.square_mirror(i)] 
-                                        for i in board.pieces(chess.BISHOP, chess.BLACK)])
-        rooksq = sum([rookstable[i] for i in board.pieces(chess.ROOK, chess.WHITE)]) 
-        rooksq = rooksq + sum([-rookstable[chess.square_mirror(i)] 
-                                        for i in board.pieces(chess.ROOK, chess.BLACK)])
-        queensq = sum([queenstable[i] for i in board.pieces(chess.QUEEN, chess.WHITE)]) 
-        queensq = queensq + sum([-queenstable[chess.square_mirror(i)] 
-                                        for i in board.pieces(chess.QUEEN, chess.BLACK)])
-        kingsq = sum([kingstable[i] for i in board.pieces(chess.KING, chess.WHITE)]) 
-        kingsq = kingsq + sum([-kingstable[chess.square_mirror(i)] 
-                                        for i in board.pieces(chess.KING, chess.BLACK)])
-    
-        eval = material + pawnsq + knightsq + bishopsq + rooksq + queensq + kingsq
-
-        if board.turn:
-            return eval
-        else:
-            return -eval
-
-    def negaMax(self, depth):
-        bestMove = chess.Move.null()
-        bestValue = float('-inf')
-        if (depth == self.maxDepth):
-            return [self.evaluate()]
-        for move in self.board.legal_moves:
-            self.board.push(move)
-            result = self.negaMax(depth + 1)
-            boardValue = -result[0]
-            if boardValue > bestValue:
-                bestValue = boardValue
-                bestMove = move
-            self.board.pop()
-        return [bestValue, bestMove]
-    
-    def negaScout(self, depth, alpha, beta):
-        bestMove = chess.Move.null()
-        bestValue = float('-inf')
-        if (depth == self.maxDepth):
-            return [self.evaluate()]
-        for move in self.board.legal_moves:
-            self.board.push(move)
-            result = self.negaScout(depth + 1, -(alpha+1), -alpha)
-            boardValue = -result[0]
-            if (boardValue > alpha and boardValue < beta and depth != self.maxDepth-1):
-                result2 = self.negaScout(depth+1, -beta, -boardValue)
-                boardValue = max(boardValue, -result2[0])
-            if boardValue > bestValue:
-                bestValue = boardValue
-                bestMove = move
-            self.board.pop()
-        return [bestValue, bestMove]
-
-    def pvs(self, depth, alpha, beta):
-        bestMove = chess.Move.null()
-        bestValue = float('-inf')
-        if (depth == self.maxDepth):
-            return [self.evaluate()]
-        for move in self.board.legal_moves:
-            self.board.push(move)
-            result = self.pvs(depth + 1, -(alpha+1), -alpha)
-            boardValue = -result[0]
-            if (boardValue > alpha and boardValue < beta):
-                result2 = self.pvs(depth+1, -beta, -alpha)
-                boardValue = -result2[0]
-            if boardValue > bestValue:
-                bestValue = boardValue
-                bestMove = move
-            self.board.pop()
-        return [bestValue, bestMove]
 
 class GameEngine:
     def __init__(self, board: chess.Board):
@@ -121,14 +14,16 @@ class GameEngine:
         
         self.board.push_san(play)
 
-    def playAIMove(self, maxDepth, color, method, alpha, beta):
-        engine = AIEngine(self.board, maxDepth, color)
+    def playAIMove(self, maxDepth, color, method):
         if(method=='negamax'):
-            bestMove = engine.negaMax(0)[1]
+            negamax = NegamaxAgent(self.board, color, maxDepth)
+            bestMove = negamax.getMove()
         elif(method=='negascout'):
-            bestMove = engine.negaScout(0, alpha, beta)[1]
+            negascout = NegascoutAgent(self.board, color, maxDepth)
+            bestMove = negascout.getMoveAB()
         else:
-            bestMove = engine.pvs(0, alpha, beta)[1]
+            pvs = PVSAgent(self.board, color, maxDepth)
+            bestMove = pvs.getMoveAB()
             
             
         print('BEST MOVE', bestMove)
@@ -140,8 +35,6 @@ class GameEngine:
         print("The game started!")
         print("You play WHITE!")
         maxDepth = 3
-        alpha = float('-inf')
-        beta = float('inf')
         turn = chess.WHITE
         while (not self.board.is_checkmate()):
             print(self.board)
@@ -152,7 +45,7 @@ class GameEngine:
                 continue
             if turn == chess.BLACK:
                 print('\n\nBlack move\n\n')
-                self.playAIMove(maxDepth, aiColor, method, alpha, beta)
+                self.playAIMove(maxDepth, aiColor, method)
                 turn = chess.WHITE
                 continue
         print(self.board)
